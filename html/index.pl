1d03275321c5dcf95169eba5688c53e5fa5485e3
root <root@noclab.com.au>
2016-10-29 00:28:48 +1100

    Rolled new* files over old v1 files

--- html/index.pl
+++ html/index.pl
@@ -1,15 +1,11 @@
 #!/usr/bin/perl
 
-# Safe Typeing:
+use strict;
+no strict "subs";
 use warnings;
 
-## Global Config ##
-
-# Our DB Module:
-use DBI;
-my $dbh = DBI->connect("dbi:SQLite:dbname=navua.db");
-
-# Our YAML Parsing + HTML Templating Engine:
+use NavuaAO;
+use CGI;
 use HTML::Template;
 use YAML::XS 'LoadFile';
 
@@ -17,144 +13,156 @@ use YAML::XS 'LoadFile';
 my $config = LoadFile('config.yaml');
 my $secret_config = LoadFile('secret.yaml');
 
-# CGI Module:
-use CGI;
+# Init CGI Module:
 my $q = CGI->new();
 
 # Extract our dates:
-my $date = `date "+%F"`;
-( my $year ) = $date =~ /([0-9]{4})-[0-9]{2}-[0-9]{2}/; 
-( my $month ) = $date =~ /[0-9]{4}-([0-9]{2})-[0-9]{2}/; 
-( my $day ) = $date =~ /[0-9]{4}-[0-9]{2}-([0-9]{2})/; 
+my $dateHash = {};
+$dateHash->{fullDate} = `date "+%F"`;
+chomp($dateHash->{fullDate});
+( $dateHash->{calYear} ) = $dateHash->{fullDate} =~ /([0-9]{4})-[0-9]{2}-[0-9]{2}/;
+( $dateHash->{calMonth} ) = $dateHash->{fullDate} =~ /[0-9]{4}-([0-9]{2})-[0-9]{2}/;
+( $dateHash->{calDay} ) = $dateHash->{fullDate} =~ /[0-9]{4}-[0-9]{2}-([0-9]{2})/;
 
-# Generate our logical dates:
-my ( $lday, $lmonth, $lyear ) = ( $day, $month, $year );
-my $logicalMonthMode = 1;
-generateLogicalDates();
-
-#print "$lmonth\n";
-#exit 1;
-
-# Convert Numerical Values to Text Value for Month:
 my %numMonth = qw(
   01 Jan  02 Feb  03 Mar  04 Apr  05 May  06 Jun
   07 Jul  08 Aug  09 Sep  10 Oct  11 Nov  12 Dec
 );
 
-## End Global ##
+sub getCurrentPayCycle {
 
-# Notify parties that an amount has been payed on the mortgage:
-sub notifyPayment{
+	# Calculate our start of cycle:
+	$dateHash->{cycDay} = 15;
+	$dateHash->{cycMonth} = $dateHash->{calMonth};
+	$dateHash->{cycYear} = $dateHash->{calYear};
 
-	# Check how much we've payed:
-	my $inputPay = shift;
+	if ( $dateHash->{calDay} < $config->{payDay} ) {
+		$dateHash->{cycMonth} = sprintf("%02d", $dateHash->{calMonth} - 1);
+		if ( $dateHash->{cycMonth} < 1 ) {
+			$dateHash->{cycMonth} = 12;
+			$dateHash->{cycYear} = sprintf("%02d", $dateHash->{calYear} - 1);
+		}
+	}
+	$dateHash->{cycStart} = "$dateHash->{cycYear}" . "-" . "$dateHash->{cycMonth}" . "-" . "$dateHash->{cycDay}";
 
-	# Generate our new HTML (after updating payed amount):
-	my $htmlOutput = `REQUEST_METHOD="GET" GEN_EMAIL="YES" ./index.pl`;
-
-	# Leverage Sendmail to send our notification email to respective parties:
-	open  (SENDMAIL, "|/usr/sbin/sendmail -f '$config->{senderEmail}' -t");
-	print SENDMAIL "From: $config->{senderEmail}\n";
-	print SENDMAIL "To: $secret_config->{destinationEmails}\n";
-	print SENDMAIL "MIME-Version: 1.0\n";
-	print SENDMAIL "Subject: \$$inputPay Payed Off Mortgage!\n";
-	print SENDMAIL "$htmlOutput";
-	close (SENDMAIL);
+	# Calculate our end of cycle:
+	$dateHash->{cycDayEnd} = 14;
+	$dateHash->{cycYearEnd} = $dateHash->{cycYear};
+	$dateHash->{cycMonthEnd} = sprintf("%02d", $dateHash->{cycMonth} + 1);
+	# Roll over:
+	if ( $dateHash->{cycMonthEnd} > 12 ) {
+		$dateHash->{cycMonthEnd} = sprintf("%02d", 1);
+		$dateHash->{cycYearEnd} = sprintf("%02d", $dateHash->{cycYear} + 1);
+	}
+	$dateHash->{cycEnd} = "$dateHash->{cycYearEnd}" . "-" . "$dateHash->{cycMonthEnd}" . "-" . "$dateHash->{cycDayEnd}";
 }
 
-# Calculate the amount of Offset paid till date:
-sub calculateOffset {
-
-	# Prepare our query:
-	my $query = "SELECT SUM(amount) FROM payments";
-	my $sqlQuery = $dbh->prepare("$query");
-	$sqlQuery->execute();
-
-	# Do the maths:
-	my $offsetAmount = $sqlQuery->fetchrow();
-	my $mortgageRemaining = $config->{"totalMortgage"} - $offsetAmount;
-
-	return ( $offsetAmount, $mortgageRemaining );
-}
-
-# Calculate current month of mortgage:
 sub calculateDuration {
 
 	# Calculate our months since we started this whole mortgage payment thing:
 	( my $startYear ) = $config->{"startDate"} =~ /([0-9]{4})-[0-9]{2}-[0-9]{2}/; 
 	( my $startMonth ) = $config->{"startDate"} =~ /[0-9]{4}-([0-9]{2})-[0-9]{2}/; 
 
-	my $diffYears = $lyear - $startYear;
-	my $diffMonths = $lmonth - $startMonth;
+	my $diffYears = $dateHash->{cycYear} - $startYear;
+	my $diffMonths = $dateHash->{cycMonth} - $startMonth;
 	my $monthsPassed = ( $diffYears * 12 ) + $diffMonths;
 
-	return ( $startYear, $startMonth, $monthsPassed );
+	#return ( $startYear, $startMonth, $monthsPassed );
+	return $monthsPassed
 }
 
-# Generate our Logical Dates, if logicalMonthMode == 1
-# This aligns our dates with our PayDate/YAML defined pay cycle:
-sub generateLogicalDates {
-	if ( $logicalMonthMode == 1 ) {
-		if ( $day < $config->{"payDay"} ) {
-			$lmonth = sprintf("%02d", $lmonth - 1);
-			# Wrap around to the previous year if nessesary:
-			if ( $lmonth < 1 ) {
-				$lmonth = "12";
-				$lyear = $lyear - 1;
-			}
+sub getRollingHistory {
+
+	my $historyDepth = 6;
+	my $navuaAO = shift;
+	my $avref = shift;
+	my $liferef = shift;
+	my $monthsPassed = shift;
+	my $rollingHistory;
+	my $i = 0;
+	
+	while ( $i < $historyDepth ) {
+		my $history = {};
+		
+		#Calculate Start:
+		$history->{cycDay} = $dateHash->{cycDay};
+		$history->{cycYear} = $dateHash->{cycYear};
+			
+		if ( checkPaymentNeeded($navuaAO) == 0 ) {
+			$history->{cycMonth} = sprintf("%02d", $dateHash->{cycMonth} - ($historyDepth - $i - 1)); 
+		# We have to make a payment, so lets go back one more:
+		} else {
+			$history->{cycMonth} = sprintf("%02d", $dateHash->{cycMonth} - ($historyDepth - $i));
 		}
+		if ( $history->{cycMonth} < 1 ) {
+			$history->{cycMonth} = 12;
+                        $history->{cycYear} = sprintf("%02d", $dateHash->{calYear} - 1);
+		}
+		
+		#Calculate End:
+		$history->{cycDayEnd} = 14;
+		$history->{cycYearEnd} = $dateHash->{cycYear};
+		$history->{cycMonthEnd} = sprintf("%02d", $history->{cycMonth} + 1);
+		# Roll over:
+		if ( $history->{cycMonthEnd} > 12 ) {
+			$history->{cycMonthEnd} = sprintf("%02d", 1);
+			$history->{cycYearEnd} = sprintf("%02d", $history->{cycYear} + 1);
+		}
+
+		$history->{cycStart} = "$history->{cycYear}" . "-" . "$history->{cycMonth}" . "-" . "$history->{cycDay}";
+		$history->{cycEnd} = "$history->{cycYearEnd}" . "-" . "$history->{cycMonthEnd}" . "-" . "$history->{cycDayEnd}";
+		if ( $history->{cycMonth} eq $dateHash->{cycMonth} ) {
+			$history->{monthName} = $numMonth{"$history->{cycMonth}"} . " (Current)";
+		} else {
+			$history->{monthName} = $numMonth{"$history->{cycMonth}"};
+		}
+
+		$history->{sum} = $navuaAO->getOffsetPaidCycle("$history->{cycStart}", "$history->{cycEnd}");
+		$$avref = $$avref + $history->{sum};
+		$history->{sum} = getSignedValue($history->{sum});
+
+		unshift(@$rollingHistory, $history);
+		$i++;
 	}
+
+	$$avref = getSignedValue(sprintf("%02d", ( $$avref / $historyDepth )));
+	$$liferef = getSignedValue($navuaAO->getLifeAverage($$monthsPassed));
+
+	return $rollingHistory;
 }
 
-# Calculate the amount to date that we have payed this month:
-sub amountPayedThisMonth {
+sub notifyPayment{
 
-	# Calculate the amount payed this month:
+        # Check how much we've payed:
+        my $inputPay = shift;
 
-	#my $query = "SELECT SUM(amount) FROM payments WHERE date LIKE '%$lyear-$lmonth%'";
-	my $cutoffDay = $config->{payDay} - 1;
-	my $query = "SELECT SUM(amount) FROM payments WHERE date BETWEEN '$lyear-$lmonth-$config->{payday}' AND '$year-$month-$cutoffDay'";
-	my $sqlQuery = $dbh->prepare("$query");
-	$sqlQuery->execute();
+        # Generate our new HTML (after updating payed amount):
+        my $htmlOutput = `REQUEST_METHOD="GET" GEN_EMAIL="YES" ./index.pl`;
 
-	my $payedThisMonth = $sqlQuery->fetchrow();
-	if ( ! defined $payedThisMonth ) {
-		$payedThisMonth = "0.00";
-	}
-
-	return $payedThisMonth;
+        # Leverage Sendmail to send our notification email to respective parties:
+        open  (SENDMAIL, "|/usr/sbin/sendmail -f '$config->{senderEmail}' -t");
+        print SENDMAIL "From: $config->{senderEmail}\n";
+        print SENDMAIL "To: $secret_config->{destinationEmails}\n";
+        print SENDMAIL "MIME-Version: 1.0\n";
+        print SENDMAIL "Subject: \$$inputPay Payed Off Mortgage!\n";
+        print SENDMAIL "$htmlOutput";
+        close (SENDMAIL);
 }
 
-# Add a payment into the offset / against the mortgage:
-sub addPayment {
-
-	chomp($date);
-	my $inputPay = $q->param("inputPay");
-	my $query = "INSERT INTO payments(date,amount) VALUES ('$date',$inputPay)";
-	my $returnVal = $dbh->do($query) or die $DBI::errstr;
-	notifyPayment("$inputPay");
-}
-
-# Check to see if it's passed payday and a payment has not been made a.k.a payment needed :) :
 sub checkPaymentNeeded {
 
 	# Check to see if we've been naughty:
-	my $paymentNeeded = 1;
+	my $navuaAO = shift;
+	my $paymentNeeded = 0;
 
 	# If it's past our payDay cutoff:
-	if ( $day >= $config->{"payDay"} ) {
+	if ( $dateHash->{calDay} >= $config->{"payDay"} ) {
 
-		# Prepare our SQL Query:
-		my $sqlQuery = $dbh->prepare("SELECT date FROM payments");
-		$sqlQuery->execute();
-
-		# If we have payed for this month, we dont need to pay again silly:
-		while ( my $row = $sqlQuery->fetchrow_hashref ) {
-			if ( $row->{"date"} =~ /$year-$month/ ) {
-				$paymentNeeded = 0;
-			}
+		# Check if we've already made a payment:
+		my $paymentMade = $navuaAO->checkPaymentsMade("$dateHash->{calYear}-$dateHash->{calMonth}");
+		if ( $paymentMade == 0 ) {
+			$paymentNeeded = 1;
 		}
-
 	# Its still early in the month, we dont need to pay up just yet:
 	} else {
 		$paymentNeeded = 0;
@@ -163,51 +171,29 @@ sub checkPaymentNeeded {
 	return $paymentNeeded;
 }
 
-# Generate our last X month history of "donations":
-sub generateRollingHistory {
+sub getSignedValue {
 
-	my $offsetAmount = shift;
-	my $monthsPassed = shift;
-
-	my @rollingHistory;
-	my $historyLimit = 6;
-	my $average = 0;
-
-	my $sqlQuery = $dbh->prepare("SELECT substr(date,6,2) AS paymonth, SUM(amount) AS paysum 
-				      FROM payments GROUP BY substr(date,0,8) ORDER BY date DESC LIMIT $historyLimit");
-	$sqlQuery->execute();
-
-	while ( my $row = $sqlQuery->fetchrow_hashref ) {
-		if ( $numMonth{"$row->{paymonth}"} =~ $numMonth{"$lmonth"} ) {
-			$row->{paymonth} = $numMonth{"$row->{paymonth}"} . " <i>(Current)</i>";
-		} else {
-			$row->{paymonth} = $numMonth{"$row->{paymonth}"};
-		}
-		$average = $average + $row->{"paysum"};
-		#$row->{"paysum"} = formatNumbers($row->{"paysum"});
-		$row->{"paysum"} = getSignedValue($row->{"paysum"});
-		push(@rollingHistory, $row);
+	my $val = shift;
+	if ( $val >= 0 ) {
+		return "+ \$" . abs($val);
+	} else {
+		return "- \$" . abs($val);
 	}
-
-	# Calculate Average Donation and Slam into an anonymous hash:
-	$average = $average / $historyLimit;
-	$average =~ s/\.[0-9]*//g;
-	unshift(@rollingHistory, { paymonth => "<b>6 Mo. Average:</b>", paysum => getSignedValue($average) });
-
-	# Calculate our total average payment for the current life of the loan:
-	my $totalAverage = $offsetAmount / $monthsPassed;
-	$totalAverage =~ s/\.[0-9]*//g;
-	unshift(@rollingHistory, { paymonth => "<b>Life Average:</b>", paysum => getSignedValue($totalAverage) });
-
-	return \@rollingHistory;
 }
 
 # Check if interest is due:
 sub checkInterestDue {
 
 	my $interest_due = 0;
-	$interest_due = 1 if $day >= $config->{payDay};
+	$interest_due = 1 if $dateHash->{calDay} >= $config->{payDay};
 	return $interest_due;
+}
+
+sub checkRentDue {
+
+	my $rent_due = 0;
+	$rent_due = 1 if $dateHash->{calDay} >= $config->{rentDay};
+	return $rent_due;
 }
 
 # Sub to format our numbers like XX,XXX etc..
@@ -241,46 +227,49 @@ sub formatNumbers {
 	return join("", @r);
 }
 
-# Sub to return correct signed dollar value
-# Not originally written in, but since life happens, sometimes you gotta go with flow:
-sub getSignedValue {
+sub Main {
 
-	my $val = shift;
-	if ( $val >= 0 ) {
-		return "+ \$" . abs($val);
-	} else {
-		return "- \$" . abs($val);
-	}
-}
+        # Define our Template Objects:
+        my $head_template = HTML::Template->new( filename => "head.tmpl" );
+        my $template = HTML::Template->new( filename => "index.tmpl", die_on_bad_params => 0 );
+        my $tail_template = HTML::Template->new( filename => "tail.tmpl" );
 
-sub Main{
+	getCurrentPayCycle();
+
+	my $navuaAO = NavuaAO->new(filename => "navua.db");
 
 	# If we were POSTed; lets update our db:
 	if ( $q->request_method eq "POST" ) {
-		addPayment();
+		$navuaAO->addOffsetPayment($dateHash->{fullDate},$q->param("inputPay"));
+		notifyPayment($q->param(inputPay));
 	}
 
-	# Define our Template Objects:
-	my $head_template = HTML::Template->new( filename => "head.tmpl" );
-	my $template = HTML::Template->new( filename => "index.tmpl", die_on_bad_params => 0 );
-	my $tail_template = HTML::Template->new( filename => "tail.tmpl" );
+	my $average = 0;
+	my $lifeAverage = 0;
+	my $monthsPassed = calculateDuration();
 
-	# Fill out and fire away:
-	my ( $offsetAmount, $mortgageRemaining ) = calculateOffset();
-	my ( $startYear, $startMonth, $monthsPassed ) = calculateDuration();
-	my $paymentNeeded = checkPaymentNeeded();
-
-	$template->param( month, $monthsPassed );
-	$template->param( paymentNeeded, $paymentNeeded );
-	$template->param( offsetAmount, formatNumbers($offsetAmount) );
-	$template->param( mortgageRemaining, formatNumbers($mortgageRemaining) );
-	$template->param( payedThisMonth, getSignedValue(amountPayedThisMonth()) );
-	$template->param( rollingHistory, generateRollingHistory($offsetAmount, $monthsPassed) );
 	$template->param( interestDue, checkInterestDue() );
+	$template->param( rentDue, checkRentDue() );
+	$template->param( lastOffsetValue, formatNumbers($navuaAO->getLastOffsetValue()) );
+	$template->param( mortgageRemaining, formatNumbers($navuaAO->getMortgageRemaining($config->{totalMortgage})) );
+	$template->param( currentOffset, formatNumbers($navuaAO->getCurrentOffset($config->{payDay})) );
+	$template->param( paymentNeeded, checkPaymentNeeded($navuaAO) );
+	$template->param( payCycleStart, $dateHash->{cycStart} );
+	$template->param( payCycleEnd, $dateHash->{cycEnd} );
+	$template->param( currentOffsetPayment, getSignedValue($navuaAO->getOffsetPaidCycle("$dateHash->{cycStart}", "$dateHash->{cycEnd}")) );
+	$template->param( rollingHistory, getRollingHistory($navuaAO, \$average, \$lifeAverage, \$monthsPassed) );
+	$template->param( monthsPassed, $monthsPassed );
+	$template->param( averagePayment, $average);
+	$template->param( lifeAverage, $lifeAverage);
+
 	$template->param( generateEmail, $ENV{GEN_EMAIL} );
 
-	# Output:
-	print "Content-Type: text/html\n\n", $head_template->output, $template->output, $tail_template->output;
+	# Diags:
+	#$template->param( diag_one, $navuaAO->checkPaymentsMade("$dateHash->{calYear}-$dateHash->{calMonth}") );
+	#$template->param( diag_two, checkPaymentNeeded($navuaAO) );
+
+        # Output:
+        print "Content-Type: text/html\n\n", $head_template->output, $template->output, $tail_template->output;
 }
 
 Main();
